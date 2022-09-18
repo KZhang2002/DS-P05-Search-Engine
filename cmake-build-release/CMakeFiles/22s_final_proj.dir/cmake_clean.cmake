6cc8fa3bede35dca115dcb363b0ab1006b4e6bd8
KZhang2002 <52335167+KZhang2002@users.noreply.github.com>
2022-09-17 20:28:25 -0500

    Copied Files 2

---
+++ cmake-build-release/CMakeFiles/22s_final_proj.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "22s_final_proj"
+  "22s_final_proj.pdb"
+  "CMakeFiles/22s_final_proj.dir/catch_setup.cpp.o"
+  "CMakeFiles/22s_final_proj.dir/main.cpp.o"
+  "CMakeFiles/22s_final_proj.dir/porter2_stemmer-master/porter2_stemmer.cpp.o"
+  "CMakeFiles/22s_final_proj.dir/search_engine.cpp.o"
+  "CMakeFiles/22s_final_proj.dir/test.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/22s_final_proj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
